9463c668007592f47313062f0bd8df4f584d937b
Arisandi123 <95145972+Arisandi123@users.noreply.github.com>
2021-12-05 01:07:42 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,27 +1,19 @@
 const allpayment = (prefix) => {
 return `PAYMENT BY ZEEONE OFC
 
-*さ BANK*
-1. BRI 
-	ㅁ 5074-01-003141-52-8
-		• R***A*DI
-		
 *さ E-MONEY*
 1. GOPAY
-	ㅁ 0887 4350 47326
-		• ZE**** **C
-2. OVO
-	ㅁ 0887 4350 47326
-		• EL*****TI
-3. DANA
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ 0888 0286 9987
+		• AriSandi
+2. DANA
+	ㅁ 0888 0286 9987
+		• AriSandi
 
 *さ PULSA*
 1. SMARTFREN
-	ㅁ 0887 4350 47326
-2. TELKOMSEL
-	ㅁ 0821 9311 5723
+	ㅁ 0888 0286 9987
+2. INDOSAT
+	ㅁ 0858 6681 6277
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
